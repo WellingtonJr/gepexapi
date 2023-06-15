0dd06af2eaf194b7fca0a5cbf894a9bc45766f3c
WellingtonJr <wferreira.bm13@gmail.com>
2023-06-14 21:31:20 -0300

    first commit

---
+++ src/main/java/com/hame/gepexapi/GepexapiApplication.java
@@ -0,0 +1,13 @@
+package com.hame.gepexapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GepexapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GepexapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hame/gepexapi/GepexapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hame.gepexapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GepexapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
